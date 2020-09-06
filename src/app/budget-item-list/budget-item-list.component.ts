8079052ec6362fa4bf63040b8fba09c050109c42
wvGithmi <wvganjana@gmail.com>
2020-09-06 23:30:09 +0530

    Delete items from the list

--- src/app/budget-item-list/budget-item-list.component.ts
+++ src/app/budget-item-list/budget-item-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { BudgetItem } from 'src/shared/models/budget-item.module';
 
 @Component({
@@ -9,10 +9,15 @@ import { BudgetItem } from 'src/shared/models/budget-item.module';
 export class BudgetItemListComponent implements OnInit {
 
   @Input() budgetItems: BudgetItem[];
+  @Output() delete: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDeleteButtonClicked(item: BudgetItem) {
+    this.delete.emit(item);
   }
 
 }
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -19,4 +19,9 @@ export class MainPageComponent implements OnInit {
     this.budgetItems.push(newItem);
   }
 
+  deleteItem(item: BudgetItem) {
+    let index = this.budgetItems.indexOf(item);
+    this.budgetItems.splice(index, 1);
+  }
+
 }
